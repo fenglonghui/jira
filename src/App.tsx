aa61d5d00b0af17e40ea9d1fcd716e5f2c467f4e
冯龙辉 <fenglongh@chanjet.com>
2022-03-30 00:47:19 +0800

    fix: 测试文件内容格式化

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-29 22:28:33
+ * @LastEditTime: 2022-03-30 00:46:29
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /jira/src/App.tsx
+ */
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
