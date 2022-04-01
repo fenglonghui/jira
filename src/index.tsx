63acf55f07b593dacbd842768462b84501f955cb
冯龙辉 <fenglongh@chanjet.com>
2022-04-01 14:13:15 +0800

    fix: 安装分布式服务jira-dev-tool

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * @Author: flh
+ * @Date: 2022-03-29 22:28:33
+ * @LastEditTime: 2022-04-01 13:57:15
+ * @LastEditors: your name
+ * @Description: 入口文件
+ * @FilePath: /jira/src/index.tsx
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { loadDevTools } from "jira-dev-tool";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+loadDevTools(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
 );
 
 // If you want to start measuring performance in your app, pass a function
